f98e821ad37a46b782c6a2a4701c28c022485138
akirovitaliXD <petrov.welel@gmail.com>
2023-08-04 20:47:47 +1000

    0.0.2

--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 server = Flask(__name__)
 @server.route("/")
- def hello():
+def hello():
     return "Hello World!"
 if __name__ == "__main__":
-   server.run()
+    server.run()
